024c3457db46bf115b5884a208520fdd7c8bde25
Minh <95263712+minhng01@users.noreply.github.com>
2021-12-16 21:47:47 +0000

    Removed repeated return null pointers

--- main.cpp
+++ main.cpp
@@ -66,10 +66,12 @@ struct Comparison                                //4
 {
     T* compare(T* a, T* b) //5
     {
-        if (a == nullptr || b == nullptr)
-            return nullptr;
-        if( a->value < b->value ) return a;
-        if( a->value > b->value ) return b;
+        if (a != nullptr && b != nullptr)
+        {
+            if( a->value < b->value ) return a;
+            if( a->value > b->value ) return b;
+        }
+        
         return nullptr;
     }
 };
